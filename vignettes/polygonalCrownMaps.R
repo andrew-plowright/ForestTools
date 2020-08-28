23fe987b612dfb74acfd1d592c92bf12114b65a0
Andrew Plowright <andrewandrew.plowright@alumni.ubc.ca>
2020-08-28 13:07:21 -0700

    Update vignettes

---
+++ vignettes/polygonalCrownMaps.R
@@ -0,0 +1,16 @@
+## ----global_options, include=FALSE, dpi =  300--------------------------------
+knitr::opts_knit$set(global.par = TRUE)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  update.packages()
+#  
+#  packageVersion("APfun")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  library(ForestTools)
+#  
+#  data("kootenayCHM")
+#  data("kootenayTrees")
+#  
+#  kootenayCrowns <- mcws(kootenayTrees, kootenayCHM, minHeight = 1.5, format = "polygons", OSGeoPath = "C:\\OSGeo4W64")
+
